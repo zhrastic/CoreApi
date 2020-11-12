42bb240588347133f173a717c3974f977a63630d
Zvonimir Hrastić <zhrastic@gmail.com>
2020-11-12 15:10:19 +0100

    Uljepšavanje

--- Models/Helpers/DataGenerator.cs
+++ Models/Helpers/DataGenerator.cs
@@ -16,13 +16,14 @@ namespace CoreApi
                     return ;
                 }
 
-                context.TodoItems.AddRange(
-                    new TodoItem() {Id = 1, Name = "Pero"},
-                    new TodoItem() {Id = 2, Name = "Štef"},
-                    new TodoItem() {Id = 3, Name = "Jura"},
-                    new TodoItem() {Id = 4, Name = "Božo"},
-                    new TodoItem() {Id = 5, Name = "Marko"}
-                );
+                context.TodoItems.AddRange(new TodoItem() { Id = 1, Name = "Pero" },
+                                           new TodoItem() { Id = 2, Name = "Štef" },
+                                           new TodoItem() { Id = 3, Name = "Jura" },
+                                           new TodoItem() { Id = 4, Name = "Božo" },
+                                           new TodoItem() { Id = 5, Name = "Luka" },
+                                           new TodoItem() { Id = 6, Name = "Peđa" },
+                                           new TodoItem() { Id = 7, Name = "Zoran" },
+                                           new TodoItem() { Id = 8, Name = "Marko" });
 
                 context.SaveChanges();
             }
